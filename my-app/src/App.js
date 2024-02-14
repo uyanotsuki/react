466ac6810454d484f7ed75a09f43f8dc1b3ff652
kageyoshi <diana.azamatovva@gmail.com>
2024-02-14 14:18:22 +0300

    1-11

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+function App() {
+  return (
+    <ul>
+      <li>пон</li>
+      <li>пончик</li>
+      <li>чай поставьте</li>
+      <li>bye bye</li>
+      <li>биополе</li>
+      <li>никахромчик</li>
+      <li>синнабончик</li>
+      <li>валорант</li>
+      <li>как апнуть ранг</li>
+      <li>я бы только спал и спал и спал</li>
+    </ul>
+  );
+}
+
+function App() {
+  return <input />;
+}
+
+export default App;
